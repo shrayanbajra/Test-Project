a1562c07f153a8f379291bcb88d34b42a0a053cb
Shrayan Bajracharya <shrayanbajracharya@gmail.com>
2021-11-02 09:16:05 +0545

    Changed animation for expanding and collapsing card view

--- app/src/main/java/com/example/testproject/ExpansibleCardFragment.kt
+++ app/src/main/java/com/example/testproject/ExpansibleCardFragment.kt
@@ -1,13 +1,16 @@
 package com.example.testproject
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.RelativeLayout
+import androidx.cardview.widget.CardView
+import androidx.fragment.app.Fragment
+import androidx.transition.AutoTransition
+import androidx.transition.TransitionManager
 
 class ExpansibleCardFragment : Fragment() {
 
@@ -29,6 +32,7 @@ class ExpansibleCardFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val cvExpansibleCardView: CardView = view.findViewById(R.id.cv_expansible_card_view)
         val rlMainView: RelativeLayout = view.findViewById(R.id.rl_main_view)
         val llDetailView: LinearLayout = view.findViewById(R.id.ll_detail_view)
         val ivArrow: ImageView = view.findViewById(R.id.iv_arrow)
@@ -38,12 +42,14 @@ class ExpansibleCardFragment : Fragment() {
             val isCardExpanded = llDetailView.visibility == View.VISIBLE
             if (isCardExpanded) {
 
-                AnimationUtils.collapse(view = llDetailView)
+                TransitionManager.beginDelayedTransition(cvExpansibleCardView, AutoTransition())
+                llDetailView.visibility = View.GONE
                 AnimationUtils.toggleArrow(view = ivArrow, isExpanded = true)
 
             } else {
 
-                AnimationUtils.expand(view = llDetailView)
+                TransitionManager.beginDelayedTransition(cvExpansibleCardView, AutoTransition())
+                llDetailView.visibility = View.VISIBLE
                 AnimationUtils.toggleArrow(view = ivArrow, isExpanded = false)
 
             }
